90139db2b14fcf7bcf99fd90811152508da3bbe6
Tomas Holas <tomas.holas@gmail.com>
2016-12-16 13:18:42 +0100

    update packages

--- controllers/candidates.js
+++ controllers/candidates.js
@@ -1,27 +1,26 @@
 var Candidate = require('../models/candidate');
+var Boom = require('boom');
 
 exports.foo = function * (request, reply) {
+  console.log(request.query)
+  if (!request.query.question) {
+    reply(Boom.create(400, 'Bad request: a must include question parameter'))
+  }
   var value = yield new Promise(function (resolve, reject) {
     setTimeout(function () {
-      resolve(42)
+      resolve(`Answer to ${request.query.question} is.. 42!`)
     }, 1000)
   })
   reply({ value: value })
 }
 
 exports.create = function * (request, reply) {
-  // console.log(request);
-  // console.log('---- payload ----')
-  // console.log(request.payload);
-  // console.log('---- parsed ----')
-  // var payload = JSON.parse(request.payload);
   console.log(request.payload);
   var result = yield Candidate.create(request.payload);
   reply({ result });
 }
 
 exports.list = function * (request, reply) {
-  // console.log(request);
   var result = yield Candidate.find();
   reply({ result });
 }
--- server.js
+++ server.js
@@ -13,7 +13,7 @@ var uristring = process.env.MONGODB_URI || 'mongodb://localhost/pipe-server';
 
 // Makes connection asynchronously.  Mongoose will queue up database
 // operations and release them when the connection is complete.
-mongoose.connect(uristring, function(err, res) {
+mongoose.connect(uristring, {poolSize: 100}, function(err, res) {
   if (err) {
     console.log('ERROR connecting to: ' + uristring + '. ' + err);
   } else {
